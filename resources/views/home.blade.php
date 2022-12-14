d503eaa64f5d1fce2f35117d3eff1a1716bf6a47
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-12-14 16:46:39 +0100

    displayeddata on the home page with for each

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
+    <style>
+        .navbar {
+            width: 100%;
+        }
+    </style>
 </head>
 
 <body>
+    <header>
+        <div class="navbar">
+            <ul>
+                @foreach($headerList as $link)
+                <li><a href="">{{ $link }}</a></li>
+                @endforeach
+            </ul>
+        </div>
+    </header>
     <h1>Hello world</h1>
+
+
 </body>
 
 </html>
